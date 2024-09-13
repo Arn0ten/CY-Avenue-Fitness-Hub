2e74d3802b244bce1478050ef2061fe66b057b98
Arn <Arn@ARNY>
2024-09-13 16:09:48 +0800

    updated design

---
+++ pages/frmMemberManagement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CY_Avenue_Fitness_Hub.pages
+{
+    public partial class frmMemberManagement : Form
+    {
+        public frmMemberManagement()
+        {
+            InitializeComponent();
+        }
+    }
+}
